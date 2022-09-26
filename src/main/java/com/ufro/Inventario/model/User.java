01396575ebadd9309bd0d5c1e780c663d8b85e17
Felipe Fuentes <f.fuentes18@ufromail.cl>
2022-09-26 10:41:52 -0300

    Documentado

--- src/main/java/com/ufro/Inventario/model/User.java
+++ src/main/java/com/ufro/Inventario/model/User.java
@@ -2,6 +2,10 @@ package com.ufro.Inventario.model;
 
 import javax.persistence.*;
 
+/**
+ * User representa a los usuarios que entran al inventario
+ * @author Felipe Fuentes
+ */
 @Entity
 public class User {
     @Id
@@ -18,33 +22,38 @@ public class User {
     @Column(name = "clave", nullable = false, length = 70)
     private String clave;
 
-    // Nota: columnDefiniton necesario ya que hibernate define automáticamente String como varchar(255)
-    @Column(name = "profile_picture", columnDefinition="TEXT")
-    private String profilePicture;
 
     public User() {
     }
 
-    public User(Long id, String nombreUsuario, String clave, String Correo) {
+    /**
+     * Obtiene un usuario con todos sus datos
+     * @param id ID unica del usuario
+     * @param nombreUsuario Nombre del usuario
+     * @param clave Clave para ingresar
+     * @param correo Correo del usuario
+     */
+    public User(Long id, String nombreUsuario, String clave, String correo) {
         this.id = id;
         this.nombreUsuario = nombreUsuario;
         this.clave = clave;
         this.correo =correo;
     }
 
+    /**
+     * Obtiene un usuario solo con su nombre y clave de acceso
+     * @param nombreUsuario Nombre del Usuario
+     * @param clave Clave de acceso
+     */
     public User(String nombreUsuario, String clave) {
         this.nombreUsuario = nombreUsuario;
         this.clave = clave;
     }
 
-    public String getProfilePicture() {
-        return profilePicture;
-    }
-
-    public void setProfilePicture(String profilePicture) {
-        this.profilePicture = profilePicture;
-    }
-
+    /**
+     * Develve la clave de un usuario
+     * @return Clave de acceso
+     */
     public String getclave() {
         return clave;
     }
@@ -53,6 +62,10 @@ public class User {
         this.clave = clave;
     }
 
+    /**
+     * Obtiene el nombre de un usuario
+     * @return Nombre de un usuario
+     */
     public String getnombreUsuario() {
         return nombreUsuario;
     }
@@ -61,6 +74,10 @@ public class User {
         this.nombreUsuario = nombreUsuario;
     }
 
+    /**
+     * Obtiene el correo de un usuario
+     * @return correo de un usuario
+     */
     public String getCorreo() {
         return correo;
     }
@@ -69,6 +86,10 @@ public class User {
         this.correo = correo;
     }
 
+    /**
+     * Retorna el identificador de un usuario
+     * @return ID del usuario
+     */
     public Long getId() {
         return id;
     }
